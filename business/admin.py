96f972a868b24ff06e5937bdfe9a82707baf35d9
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-04-06 17:09:54 +0530

    city

--- business/admin.py
+++ business/admin.py
@@ -6,54 +6,6 @@ from mptt.admin import DraggableMPTTAdmin
 
 from .models import *
 
-
-@admin_thumbnails.thumbnail('image')
-class CityAdmin(DraggableMPTTAdmin):
-    mptt_indent_field = "title"
-    list_display = ('tree_actions', 'indented_title', 'image_thumbnail',
-                    'related_locality_count','related_company_count',)
-    list_display_links = ('indented_title',)
-    list_per_page = 30 
-    search_fields = ['title']
-    prepopulated_fields = {'slug': ('title',)}
-    
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-       
-        # Add non cumulative product count
-        qs = City.objects.add_related_count(qs,
-                 Locality,
-                 'city',
-                 'locality_count',
-                 cumulative=False)
-        qs = City.objects.add_related_count(qs,
-                 Company,
-                 'city',
-                 'company_count',
-                 cumulative=False)     
-        return qs 
-
-    def related_locality_count(self, instance):
-        return instance.locality_count
-    related_locality_count.short_description = 'Related Project (for this specific Locality)'
-       
-
-    def related_company_count(self, instance):
-        return instance.company_count
-    related_company_count.short_description = 'Related Locality (for this specific City)'
-
-
-@admin_thumbnails.thumbnail('image')
-class LocalityAdmin(DraggableMPTTAdmin):
-    mptt_indent_field = "title"
-    list_display = ('id','city', 'tree_actions', 'indented_title', 'image_thumbnail','slug',
-                    )
-    list_display_links = ('indented_title',)
-    list_filter = ('title','city',) 
-
-    search_fields = ['title']
-    list_per_page = 30 
-    prepopulated_fields = {'slug': ('title',)}    
   
 class CategoryApproxInline(admin.TabularInline):
     model = Approx
@@ -169,8 +121,6 @@ class Follow_UpAdmin(admin.ModelAdmin):
 
 admin.site.register(SocialLink,SocialLinkAdmin)
 admin.site.register(Approx,ApproxAdmin)
-admin.site.register(City,CityAdmin)
-admin.site.register(Locality,LocalityAdmin)
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Company,CompanyAdmin)
 
