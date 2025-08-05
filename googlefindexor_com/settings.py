c06e8e730d5e6e969430978e6bcd0ad5d11236c2
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-08-05 18:57:05 +0530

    Kolkata

--- googlefindexor_com/settings.py
+++ googlefindexor_com/settings.py
@@ -85,6 +85,9 @@ DATABASES = {
     }
 }
 
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -110,7 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -124,8 +128,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 
 # ...
 SITE_ID = 1
