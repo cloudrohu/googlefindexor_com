f837f8f1edd486fbef372df7bd5cf7f05d9ed50b
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-09-10 10:57:50 +0530

    followup

--- response/admin.py
+++ response/admin.py
@@ -5,14 +5,12 @@ class MeetingInline(admin.TabularInline):
     model = Meeting
     extra = 1
     show_change_link = True
-
     exclude = ['created_by', 'updated_by']
 
 class FollowupInline(admin.TabularInline):
     model = Followup
     extra = 1
     show_change_link = True
-
     exclude = ['created_by', 'updated_by']
 
 
@@ -32,76 +30,175 @@ class ResponseAdminForm(forms.ModelForm):
 
 
 class ResponseAdmin(admin.ModelAdmin):
-    list_display = ['id','status','contact_no','comment', 'contact_persone', 'business_name','update_at','create_at','created_by','updated_by']    
-    list_filter = ['status']
-    list_editable = ['status']
+    list_display = [
+        'get_mr_id',   # <-- yahan custom MR id show karenge
+        'status',
+        'contact_no',
+        'comment',
+        'contact_persone',
+        'business_name',
+        'business_category',
+        'locality_city',
+        'city',
+        'update_at',
+        'create_at',
+        'created_by',
+        'updated_by'
+    ]    
+    list_filter = ['status','locality_city','city','business_category',]
+    list_editable = ['status','locality_city','city','business_category',]
     search_fields = ['id','contact_no', 'comment']
     list_per_page = 15
-    inlines = [MeetingInline,FollowupInline]
+    inlines = [MeetingInline, FollowupInline]
 
-    def save_model(self, request, obj, form, change):
-        if not change:
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        super().save_model(request, obj, form, change)
-
-    form = ResponseAdminForm
-
-
-    exclude = ['created_by', 'updated_by']
-
-    # Auto fill user
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:  # New object
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        super().save_model(request, obj, form, change)
+    # Custom MR id
+    def get_mr_id(self, obj):
+        return f"MR{obj.id}"
+    get_mr_id.short_description = "ID"   # column heading
 
 class MeetingAdmin(admin.ModelAdmin):
-    list_display = [
-        'id','response','meeting', 'description','locality_city', 
-        'city','create_at','update_at', 'created_by', 'updated_by'
-    ]    
-    list_filter = (
-        'meeting','locality_city',
-        'city',
-        'create_at','update_at'
+
+    list_display = (
+        "id", 
+        "response", 
+        "get_response_contact_no", 
+        "meeting", 
+        "get_response_comment",
+        "get_response_business_name",
+        "get_response_business_category",
+        "get_response_locality",
+        "get_response_city",
+        "get_response_create_at",
+        "get_response_update_at", 
+        "get_response_created_by", 
+        "get_response_updated_by"
     )
-    list_editable = ['meeting','locality_city', 
-        'city']
+    list_filter = ("meeting","response__city", "response__locality_city","response__business_category",)
+    search_fields = ("response__id", "response__business_name", "response__contact_no")
+    list_editable = ["meeting"]
     list_per_page = 15
 
+    def get_response_comment(self, obj):
+        return obj.response.comment if obj.response else "-"
+    get_response_comment.short_description = "Response Comment"
 
-    # Auto fill user
+    def get_response_contact_no(self, obj):
+        return obj.response.contact_no if obj.response else "-"
+    get_response_contact_no.short_description = "Response Contact No"
+
+    def get_response_city(self, obj):
+        return obj.response.city if obj.response and obj.response.city else "-"
+    get_response_city.short_description = "City"
+
+    def get_response_locality(self, obj):
+        return obj.response.locality_city if obj.response and obj.response.locality_city else "-"
+    get_response_locality.short_description = "Locality"
+
+    def get_response_business_name(self, obj):
+        return obj.response.business_name if obj.response and obj.response.business_name else "-"
+    get_response_business_name.short_description = "Business Name"
+
+    def get_response_business_category(self, obj):
+        return obj.response.business_category if obj.response and obj.response.business_category else "-"
+    get_response_business_category.short_description = "Business Category"
+
+    def get_response_create_at(self, obj):
+        return obj.response.create_at.strftime("%d-%m-%Y %H:%M") if obj.response and obj.response.create_at else "-"
+    get_response_create_at.short_description = "Response Created At"
+
+    def get_response_update_at(self, obj):
+        return obj.response.update_at.strftime("%d-%m-%Y %H:%M") if obj.response and obj.response.update_at else "-"
+    get_response_update_at.short_description = "Response Updated At"
+
+    def get_response_created_by(self, obj):
+        return obj.response.created_by.username if obj.response and obj.response.created_by else "-"
+    get_response_created_by.short_description = "Created By"
+
+    def get_response_updated_by(self, obj):
+        return obj.response.updated_by.username if obj.response and obj.response.updated_by else "-"
+    get_response_updated_by.short_description = "Updated By"
+
+    # Auto fill user (agar future me Meeting me bhi created_by chahiye ho)
     def save_model(self, request, obj, form, change):
-        if not obj.pk:  # New object
+        if not obj.pk and hasattr(obj, 'created_by'):
             obj.created_by = request.user
-        obj.updated_by = request.user
+        if hasattr(obj, 'updated_by'):
+            obj.updated_by = request.user
         super().save_model(request, obj, form, change)
 
 
+        
 
 class FollowupAdmin(admin.ModelAdmin):
-    list_display = [
-        'id','response','followup', 'description','locality_city', 
-        'city','create_at','update_at', 'created_by', 'updated_by'
-    ]    
-    list_filter = (
-        'followup','locality_city',
-        'city',
-        'create_at','update_at'
+       
+    list_display = (
+        "id", 
+        "response", 
+        "get_response_contact_no", 
+        "followup", 
+        "get_response_comment",
+        "get_response_business_name",
+        "get_response_business_category",
+        "get_response_locality",
+        "get_response_city",
+        "get_response_create_at",
+        "get_response_update_at", 
+        "get_response_created_by", 
+        "get_response_updated_by"
     )
-    list_editable = ['followup','locality_city', 
-        'city']
+    list_filter = ("followup","response__city", "response__locality_city","response__business_category",)
+    search_fields = ("response__id", "response__business_name", "response__contact_no")
+    list_editable = ["followup"]
     list_per_page = 15
 
+    def get_response_comment(self, obj):
+        return obj.response.comment if obj.response else "-"
+    get_response_comment.short_description = "Response Comment"
 
-    # Auto fill user
+    def get_response_contact_no(self, obj):
+        return obj.response.contact_no if obj.response else "-"
+    get_response_contact_no.short_description = "Response Contact No"
+
+    def get_response_city(self, obj):
+        return obj.response.city if obj.response and obj.response.city else "-"
+    get_response_city.short_description = "City"
+
+    def get_response_locality(self, obj):
+        return obj.response.locality_city if obj.response and obj.response.locality_city else "-"
+    get_response_locality.short_description = "Locality"
+
+    def get_response_business_name(self, obj):
+        return obj.response.business_name if obj.response and obj.response.business_name else "-"
+    get_response_business_name.short_description = "Business Name"
+
+    def get_response_business_category(self, obj):
+        return obj.response.business_category if obj.response and obj.response.business_category else "-"
+    get_response_business_category.short_description = "Business Category"
+
+    def get_response_create_at(self, obj):
+        return obj.response.create_at.strftime("%d-%m-%Y %H:%M") if obj.response and obj.response.create_at else "-"
+    get_response_create_at.short_description = "Response Created At"
+
+    def get_response_update_at(self, obj):
+        return obj.response.update_at.strftime("%d-%m-%Y %H:%M") if obj.response and obj.response.update_at else "-"
+    get_response_update_at.short_description = "Response Updated At"
+
+    def get_response_created_by(self, obj):
+        return obj.response.created_by.username if obj.response and obj.response.created_by else "-"
+    get_response_created_by.short_description = "Created By"
+
+    def get_response_updated_by(self, obj):
+        return obj.response.updated_by.username if obj.response and obj.response.updated_by else "-"
+    get_response_updated_by.short_description = "Updated By"
+
+    # Auto fill user (agar future me Meeting me bhi created_by chahiye ho)
     def save_model(self, request, obj, form, change):
-        if not obj.pk:  # New object
+        if not obj.pk and hasattr(obj, 'created_by'):
             obj.created_by = request.user
-        obj.updated_by = request.user
+        if hasattr(obj, 'updated_by'):
+            obj.updated_by = request.user
         super().save_model(request, obj, form, change)
+
 
 admin.site.register(Followup, FollowupAdmin)
 admin.site.register(Meeting, MeetingAdmin)
--- response/models.py
+++ response/models.py
@@ -20,6 +20,10 @@ class Response(models.Model):
     comment = models.CharField(max_length=500, null=True, blank=True)
     contact_persone = models.CharField(max_length=500,blank=True, null=True,)
     business_name = models.CharField(max_length=500,blank=True, null=True,)
+    business_category = models.ForeignKey(Category,blank=True, null=True , on_delete=models.CASCADE)
+    requirement_types = models.ManyToManyField('utility.RequirementType',blank=True,related_name='meetings' )  
+    city = models.ForeignKey(City,blank=True, null=True , on_delete=models.CASCADE)
+    locality_city= models.ForeignKey(Locality,blank=True, null=True , on_delete=models.CASCADE)
 
 
     create_at = models.DateTimeField(auto_now_add=True)
@@ -43,73 +47,33 @@ class Response(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.contact_no or ''} {self.comment or ''} {self.business_name or ''}"
+        return f"MR{str(self.id).zfill(3)}"
+
 
     class Meta:
         verbose_name_plural = '1. Response'
 
 # -------------------------------------------------------------------------------------------------------------
 class Meeting(models.Model):
-    meeting= models.DateTimeField(blank=True, null=True,)
-    description = models.CharField(max_length=500, null=True, blank=True)
-    business_category = models.ForeignKey(Category,blank=True, null=True , on_delete=models.CASCADE)
-    requirement_types = models.ManyToManyField('utility.RequirementType',blank=True,related_name='meetings' )
-    city = models.ForeignKey(City,blank=True, null=True , on_delete=models.CASCADE)
-    locality_city= models.ForeignKey(Locality,blank=True, null=True , on_delete=models.CASCADE)
-    response = models.ForeignKey(Response,blank=True, null=True , on_delete=models.CASCADE)
-
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
-
-    
-    created_by = models.ForeignKey(
-        User, related_name='meeting_created',
-        on_delete=models.SET_NULL, null=True, blank=True
-    )
-    updated_by = models.ForeignKey(
-        User, related_name='meeting_updated',
-        on_delete=models.SET_NULL, null=True, blank=True
-    )
+    meeting= models.DateTimeField(blank=True, null=True,)      
+    response = models.ForeignKey(Response,blank=True, null=True , on_delete=models.CASCADE)   
 
     def __str__(self):
-        return self.description 
+        return self.meeting.strftime("%d-%m-%Y %H:%M") if self.meeting else f"Meeting {self.id}" 
     
     class Meta:
         verbose_name_plural='2. Meeting'
-
-
-
 # -------------------------------------------------------------------------------------------------------------
 class Followup(models.Model):
     followup= models.DateTimeField(blank=True, null=True,)
-    description = models.CharField(max_length=500, null=True, blank=True)
-    business_category = models.ForeignKey(Category,blank=True, null=True , on_delete=models.CASCADE)
-    requirement_types = models.ManyToManyField('utility.RequirementType',blank=True,related_name='followups' )
-    city = models.ForeignKey(City,blank=True, null=True , on_delete=models.CASCADE)
-    locality_city= models.ForeignKey(Locality,blank=True, null=True , on_delete=models.CASCADE)
-    response = models.ForeignKey(Response,blank=True, null=True , on_delete=models.CASCADE)
-
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
-
-    
-    created_by = models.ForeignKey(
-        User, related_name='followup_created',
-        on_delete=models.SET_NULL, null=True, blank=True
-    )
-    updated_by = models.ForeignKey(
-        User, related_name='followup_updated',
-        on_delete=models.SET_NULL, null=True, blank=True
-    )
+    response = models.ForeignKey(Response,blank=True, null=True , on_delete=models.CASCADE)   
 
     def __str__(self):
-        return self.description 
+        return self.followup.strftime("%d-%m-%Y %H:%M") if self.followup else f"followup {self.id}" 
+   
     
     class Meta:
         verbose_name_plural='3.Follow_Up'
-
-
-
 
 
  
