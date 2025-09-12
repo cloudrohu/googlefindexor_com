fc60ff2a22a535a4ec1d7375e474b42f02fa1468
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-09-12 16:54:48 +0530

    virar west

--- utility/admin.py
+++ utility/admin.py
@@ -8,8 +8,21 @@ from mptt.admin import DraggableMPTTAdmin
 from .models import *
 
 admin.site.register(City,)
-admin.site.register(Locality,)
+
+class LocalityAdmin(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('id','tree_actions', 'indented_title', 'slug',
+                    )
+    list_display_links = ('indented_title',)
+    
+    list_per_page = 30 
+    prepopulated_fields = {'slug': ('title',)}    
+
+
+admin.site.register(Locality,LocalityAdmin)
 admin.site.register(Sub_Locality,)
+
+
 admin.site.register(Find_Form,)
 admin.site.register(Call_Status,)
 admin.site.register(SocialSite,)
