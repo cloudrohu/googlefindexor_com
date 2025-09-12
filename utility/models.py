2a9d5058f3c4b1f8d5865f0aa2def0a9c7a6e3c0
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-09-12 16:32:57 +0530

    mmt

--- utility/models.py
+++ utility/models.py
@@ -22,7 +22,6 @@ class Find_Form(models.Model):
     class Meta:
         verbose_name_plural='1. Find_Form'
 
-
 class Googlemap_Status(models.Model):    
     title = models.CharField(max_length=500,blank=True, null=True,)
     create_at=models.DateTimeField(auto_now_add=True)
@@ -34,7 +33,6 @@ class Googlemap_Status(models.Model):
     class Meta:
         verbose_name_plural='4. Googlemap_Status'
 
-
 class Call_Status(models.Model):
     title = models.CharField(max_length=500,blank=True, null=True,)
     create_at=models.DateTimeField(auto_now_add=True)
@@ -45,8 +43,6 @@ class Call_Status(models.Model):
 
     class Meta:
         verbose_name_plural='2. Call_Status'
-
-
 
 class SocialSite(models.Model):
     title = models.CharField(max_length=50,unique=True)   
@@ -67,14 +63,47 @@ class City(models.Model):
     def __str__(self):
         return self.title
 
-class Locality(models.Model):
-    city = models.ForeignKey(City, on_delete=models.CASCADE,null=True,blank=True) #many to one relation with Brand
-    title = models.CharField(max_length=500,unique=True)    
-    create_at=models.DateTimeField(auto_now_add=True)
-    update_at=models.DateTimeField(auto_now=True)
+
+class Locality(MPTTModel):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    parent = TreeForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(default="", blank=True)
+    keywords = models.CharField(max_length=255, default="", blank=True)
+    status=models.CharField(max_length=10, choices=STATUS, blank=True)
+    slug = models.SlugField(unique=True , null=True , blank=True)
+    
 
     def __str__(self):
-        return self.title + ' ' + self.city.title
+        return self.title
+    
+    def save(self , *args , **kwargs):
+        self.slug = slugify(self.title)
+        super(Locality ,self).save(*args , **kwargs)
+    
+    
+    def image_tag(self):
+        if self.image.url is not None:
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+        else:
+            return ""
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    def get_absolute_url(self):
+        return reverse('locality_detail', kwargs={'slug': self.slug})
+
+    def __str__(self):                           # __str__ method elaborated later in
+        full_path = [self.title]                  # post.  use __unicode__ in place of
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
 
 
 class Sub_Locality(models.Model):
@@ -84,8 +113,6 @@ class Sub_Locality(models.Model):
     update_at=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title + ' ' + self.locality.title + ' ' + self.locality.city.title
-    
-
 
 class Meeting_Followup_Type(models.Model):
     title = models.CharField(max_length=100,unique=True)    
@@ -94,7 +121,6 @@ class Meeting_Followup_Type(models.Model):
 
     def __str__(self):
         return self.title
-    
 
 class RequirementType(models.Model):
     name = models.CharField(max_length=100,unique=True)
@@ -106,11 +132,6 @@ class RequirementType(models.Model):
     
     class Meta:
         verbose_name_plural='4. Requirement_Type'
-
-
-
-
-
 
 class Response_Status(models.Model):
     name = models.CharField(max_length=100,unique=True)
